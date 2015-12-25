365634b8298a4defba8a660a43ba300688c4123f
WajidKagzi <wjkagzi@gmail.com>
2015-12-25 11:57:27 -0500

    Added email index on user and deleted fixtures.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@
 #
 
 class User < ActiveRecord::Base
+  # self is not optional on assigning variables.
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   # All capitals means constants.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
