413306756aa86b0d1dd05259b6c85d8a05974fee
WajidKagzi <wjkagzi@gmail.com>
2015-12-25 12:04:40 -0500

    Added test for password length and presence.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   # ActiveRecord uniqueness does not ensure DB uniqueness.
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
