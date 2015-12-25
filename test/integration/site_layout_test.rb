243dd0a7cdae1cbe86e7eb48e87cf58c610a57dc
WajidKagzi <wjkagzi@gmail.com>
2015-12-25 11:21:43 -0500

    Testing our regex for email validation.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
+  # This is a step by step way to test the views.
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    # On the root path there must be 2 ahrefs.
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
@@ -12,4 +13,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'title', full_title('Sign up')
   end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,11 +32,31 @@ class UserTest < ActiveSupport::TestCase
 
   test 'name should not be too long' do
     @user.name = 'a' * 51
+    # Assert that a user is not valid.
     assert_not @user.valid?
   end
 
   test 'email should not be too long' do
     @user.email = 'a' * 244 + '@example.com'
     assert_not @user.valid?
+  end
+
+  # Here we give a sample of valid email addresses and the test should accept them.
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com UESR@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      # The assert method has an optional second argument for an error message.
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # Here we reject invalid email addresses and test our regex in the model.
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
