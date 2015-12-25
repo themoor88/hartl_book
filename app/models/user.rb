fab3877c93ad08717d39e89c55d92bbcd38a525c
WajidKagzi <wjkagzi@gmail.com>
2015-12-24 19:14:09 -0500

    Testing validations and length parameters for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessor :name, :email
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
