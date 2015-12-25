900afa46c0bdb81883df2dec47002ea31896d806
WajidKagzi <wjkagzi@gmail.com>
2015-12-25 11:29:39 -0500

    All test passing, added uniqueness test for emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   # All capitals means constants.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  # ActiveRecord uniqueness does not ensure DB uniqueness.
 end
