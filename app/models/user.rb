6825616aa95d86de651af4a76df449d2384ef47f
WajidKagzi <wjkagzi@gmail.com>
2015-12-27 23:46:26 -0500

    Adding comments for email regex identifier.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   # All capitals means constants.
+  # This regex looks for the beginning of words with dots and @ symbols.
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   # ActiveRecord uniqueness does not ensure DB uniqueness.
