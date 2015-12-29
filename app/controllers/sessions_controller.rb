cdd944ef45f9767b825bb16f2a51681fcab7b43e
WajidKagzi <wjkagzi@gmail.com>
2015-12-28 22:35:46 -0500

    Adding nav links and current_user method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     # Authenticate is a method from rails that looks up the email and password.
     if user && user.authenticate(params[:session][:password])
-      render 'show'
+      log_in user
+      # Redirect to the user's profile page.
+      redirect_to user
     else
       # This fixes the flash message on redirect.
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,15 @@
 module SessionsHelper
+  # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def current_user
+    # This is idiomatically correct in ruby and is the same as current_user || User.find_by
+    @current_user || = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
