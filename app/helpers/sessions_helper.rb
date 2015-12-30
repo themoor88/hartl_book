6f8db6e0bdd81ae6c9a4c0229800a7da2cc8ce12
WajidKagzi <wjkagzi@gmail.com>
2015-12-29 22:11:48 -0500

    Adding comments to better understand.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,7 @@
 module SessionsHelper
   # Logs in the given user.
   def log_in(user)
+    # Add another key value pair to the session hash that identifies the user.
     session[:user_id] = user.id
   end
 
@@ -9,6 +10,7 @@ module SessionsHelper
     @current_user || = User.find_by(id: session[:user_id])
   end
 
+  # A method that checks if the user is logged in and returns true if it is the case.
   def logged_in?
     !current_user.nil?
   end
