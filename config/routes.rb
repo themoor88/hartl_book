3933dd7405dde5401eee197d92ab8c8f5e71b9c1
WajidKagzi <wjkagzi@gmail.com>
2015-12-31 23:17:36 -0500

    Quick commit for comments.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  # Routes for new sessions and logging out with deletion.
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
